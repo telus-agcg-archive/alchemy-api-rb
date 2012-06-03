650cbf7cc3ac7b4dbb8c35174f9af7feefdb3001
John Allen <john@threedogconsulting.com>
2012-06-03 13:40:33 -0400

    initial commit of gem shell

---
+++ lib/alchemy-api-rb.rb
@@ -0,0 +1,9 @@
+require "alchemy-api-rb/version"
+
+module Alchemy
+  module Api
+    module Rb
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/alchemy-api-rb/version.rb
@@ -0,0 +1,7 @@
+module Alchemy
+  module Api
+    module Rb
+      VERSION = "0.0.1"
+    end
+  end
+end
