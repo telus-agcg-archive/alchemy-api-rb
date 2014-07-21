f0a429a5b54e2417a37618e02b43775e9c145383
Garrett Hunyadi <garrettsnider@me.com>
2014-07-21 18:13:26 -0400

    Support for title extraction
    Closes #11

---
+++ lib/alchemy-api/title_extraction.rb
@@ -0,0 +1,19 @@
+module AlchemyAPI
+  class TitleExtraction < Base
+    Config.add_mode :title_extraction, self
+
+    def web_method
+      "#{method_prefix}GetTitle"
+    end
+
+    private
+
+    def supported_search_types
+      [:url, :html]
+    end
+
+    def indexer
+      "title"
+    end
+  end
+end
