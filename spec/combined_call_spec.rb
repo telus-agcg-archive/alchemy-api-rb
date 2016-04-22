b60831bc652d562f394cb4ba025e570ed8b5e924
Josh Sullivan <josh@dipperstove.com>
2016-04-21 19:13:22 -0700

    Reduce char length so Rubocop settles down

--- spec/combined_call_spec.rb
+++ spec/combined_call_spec.rb
@@ -13,14 +13,14 @@ describe AlchemyAPI, 'combined_call' do
       html: '<html><body>foo bar</body></html>',
       url: 'http://www.google.com'
     }.each do |type, value|
-      [:json].each do |output_mode|
+      [:json].each do |mode|
         before do
-          AlchemyAPI::Config.output_mode = output_mode
+          AlchemyAPI::Config.output_mode = mode
         end
 
-        describe "#{type} search with #{output_mode} results" do
+        describe "#{type} search with #{mode} results" do
           it 'returns a hash of results' do
-            VCR.use_cassette("combined_call_basic_#{type}_#{output_mode}_search") do
+            VCR.use_cassette("combined_call_basic_#{type}_#{mode}_search") do
               result = subject.search(type => value)
 
               result.must_be_instance_of Hash
@@ -28,7 +28,7 @@ describe AlchemyAPI, 'combined_call' do
           end
 
           it 'includes the keyword text and relavence' do
-            VCR.use_cassette("combined_call_basic_#{type}_#{output_mode}_search") do
+            VCR.use_cassette("combined_call_basic_#{type}_#{mode}_search") do
               result = subject.search(type => value)
 
               result['totalTransactions'].wont_be_nil
