2c4dad53620bd2102f897229cb877387cddb1b6d
Alessandro Campagni <campagni@extendi.it>
2016-05-10 12:29:05 +0200

    Add support for Alchemy text identification in images

---
+++ lib/alchemy-api/image_scene_text.rb
@@ -0,0 +1,19 @@
+module AlchemyAPI
+  class ImageSceneText < Base
+    Config.add_mode :image_scene_text, self
+
+    def web_method
+      "#{method_prefix}GetRankedImageSceneText"
+    end
+
+    private
+
+    def supported_search_types
+      [:url]
+    end
+
+    def indexer
+      "sceneTextLines"
+    end
+  end
+end
